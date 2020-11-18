e8be6db7422a26ed156b8ef9b7ef03bc77f29975
bhargav <bhargavskumar51703@gmail.com>
2020-11-18 18:51:12 +0530

    changed worker.py, to be tested

--- worker.py
+++ worker.py
@@ -2,8 +2,9 @@ import socket
 import logging
 import sys
 import json
+import threading
 from pprint import pformat
-
+import time
 from job_utils.task import Task
 
 logging.basicConfig(
@@ -11,12 +12,27 @@ logging.basicConfig(
 )
 port = int(sys.argv[1])
 w_id = int(sys.argv[2])
+responsePort = 5001
+responseHost = "localhost"
 
 logging.info("Starting worker %d on port %d", w_id, port)
 
 master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
 master.bind((host, port))
+
+def waste_time(task_id,duration):
+    while duration != 0:
+        time.sleep(1)
+        duration-=1
+    logging.info("Completed task %d on worker %d",task_id,w_id)
+    response = {}
+    response["message"]="Completed task {} on worker {} %d".format(task_id,w_id)
+    response["task_id"]=task_id
+    data = json.dumps(response)
+    master.connect((responseHost,responsePort))
+    master.sendall(data.encode())
+
 master.listen()
 
 while True:
@@ -29,3 +45,10 @@ while True:
         data = json.loads(data)
         task = Task(**data)
         logging.info("Got task: %s", pformat(task))
+        #task_id, duration
+        task_id = data["task_id"]
+        duration = data["duration"]
+        logging.info("starting task %d on worker %d",task_id,w_id)
+        thread = threading.Thread(target=waste_time,args=(task_id,duration))
+        thread.start()
+
